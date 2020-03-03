0aeca5a260dea777e8bdaeaca3e41fffed20681c
ranahiren27@ <ranahiren27@gmail.com>
2020-03-03 14:43:12 +0530

    x1

--- src/Components/product.js
+++ src/Components/product.js
@@ -2,42 +2,49 @@
 /* eslint-disable no-useless-constructor */
 import React from "react";
 import "./product.css";
-import Modal from 'react-responsive-modal';
+import Modal from "react-responsive-modal";
 
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       open: false
-    }
+    };
     console.log(props.type);
   }
   onOpenModal = () => {
     this.setState({ open: true });
   };
- 
+
   onCloseModal = () => {
     this.setState({ open: false });
   };
   render() {
-    const path = this.props.src.split('/uploads/')[1];
+    const path = this.props.src.split("/uploads/")[1];
     return (
-        <div>
-            <div className="product-card" onClick={this.onOpenModal}>
-              <div>
-                <div className="product-img">
-                      <img src={`http://localhost:3000/${path}`} width="200" height="200" />
-                </div>
-                <div className="product-name">{this.props.name}</div>
-                <div className="product-details">
-                  <div className="product-price">$ {this.props.price}</div>
-                </div>
-              </div>
+      <div>
+        <div className="product-card" onClick={this.onOpenModal}>
+          <div>
+            <div className="product-img">
+              <img
+                src={`http://localhost:3000/${path}`}
+                width="200"
+                height="200"
+              />
             </div>
-            <Modal open={this.state.open} onClose={this.onCloseModal} center>
-                <img src={`http://localhost:3000/${path}`} style={{height:"100%", width:"100%"}} />
-            </Modal>
+            <div className="product-name">{this.props.name}</div>
+            <div className="product-details">
+              <div className="product-price">$ {this.props.price}</div>
+            </div>
+          </div>
         </div>
+        <Modal open={this.state.open} onClose={this.onCloseModal} center>
+          <img
+            src={`http://localhost:3000/${path}`}
+            style={{ height: "100%", width: "100%" }}
+          />
+        </Modal>
+      </div>
     );
   }
 }
